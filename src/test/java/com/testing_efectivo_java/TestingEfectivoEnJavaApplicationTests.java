deb2f73753d5eaf26d69eeaf7d2b8ae9beccdbe5
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-06 19:35:37 -0600

    first commit on testing_efectivo_en_java

---
+++ src/test/java/com/testing_efectivo_java/TestingEfectivoEnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing_efectivo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingEfectivoEnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
