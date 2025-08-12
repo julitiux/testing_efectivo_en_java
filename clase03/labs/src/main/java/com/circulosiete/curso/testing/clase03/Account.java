83dcc25a3ddb82d25498aa84f4e2f1c3e0b39c73
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 17:56:59 -0600

    Account created

---
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/Account.java
@@ -0,0 +1,9 @@
+package com.circulosiete.curso.testing.clase03;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Account {
+  public Account(UUID uuid, BigDecimal fromInitialBalance) {
+  }
+}
