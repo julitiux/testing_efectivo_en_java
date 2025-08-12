9aa5e98926d78da6b1e1bcececad296d0d3335d6
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 15:59:55 -0600

    starting clase03

---
+++ clase03/labs/src/test/java/com/circulosiete/curso/testing/clase03/TransferServiceTest.java
@@ -0,0 +1,27 @@
+package com.circulosiete.curso.testing.clase03;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class TransferServiceTest {
+
+
+  @Test
+  void transfers_between_account_and_records_event(){
+
+    var fromInitialBalance = BigDecimal.valueOf(50);
+    var toInitialBalance = BigDecimal.valueOf(10);
+    var amountToTransfer = BigDecimal.
+
+
+
+    Account from new Acc
+
+
+  }
+
+
+
+}
+<
