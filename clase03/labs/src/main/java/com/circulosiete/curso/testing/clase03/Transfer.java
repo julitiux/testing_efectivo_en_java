32c287d0d2b310ef215df5b0ee91ff5ad92eb94c
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 18:51:52 -0600

    Transfer created

---
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/Transfer.java
@@ -0,0 +1,13 @@
+package com.circulosiete.curso.testing.clase03;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Transfer(
+  UUID id,
+  UUID from,
+  UUID to,
+  BigDecimal amount,
+  LocalDateTime when) {
+}
