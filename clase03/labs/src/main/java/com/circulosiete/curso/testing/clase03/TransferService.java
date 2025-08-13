57e45d2a2c9ceeff5ffa7a69f0e8052409915297
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 18:51:37 -0600

    implement method transfer

--- clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
@@ -1,9 +1,14 @@
 package com.circulosiete.curso.testing.clase03;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class TransferService {
   public List<TransferCreated> getEvents() {
     return List.of();
+  }
+
+  public Transfer transfer(Account from, Account to, BigDecimal bigDecimal) {
+    return null;
   }
 }
