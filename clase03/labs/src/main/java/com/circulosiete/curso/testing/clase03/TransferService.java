a09ccd88bcee3e5dff049068679ad98667c04f60
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 18:04:18 -0600

    add method getEvents on TransferService

--- clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
@@ -1,4 +1,9 @@
 package com.circulosiete.curso.testing.clase03;
 
+import java.util.List;
+
 public class TransferService {
+  public List<TransferCreated> getEvents() {
+    return List.of();
+  }
 }
