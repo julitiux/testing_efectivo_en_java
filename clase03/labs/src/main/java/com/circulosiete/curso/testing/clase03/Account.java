6d10517b4220af1639c8ce6ad38aaeb31ac91159
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-12 18:01:07 -0600

    add properties uuid and balance into Account

--- clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/Account.java
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/Account.java
@@ -4,6 +4,19 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Account {
+  private final UUID uuid;
+  private final BigDecimal balance;
+
   public Account(UUID uuid, BigDecimal fromInitialBalance) {
+    this.uuid = uuid;
+    this.balance = fromInitialBalance;
+  }
+
+  public UUID getUuid() {
+    return uuid;
+  }
+
+  public BigDecimal getBalance() {
+    return balance;
   }
 }
