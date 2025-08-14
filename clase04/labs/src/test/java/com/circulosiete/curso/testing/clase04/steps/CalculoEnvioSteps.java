c358a73fa7e13fdae34237af41cf3a286b783afa
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-14 15:31:09 -0600

    add CalculoEnvioSteps

---
+++ clase04/labs/src/test/java/com/circulosiete/curso/testing/clase04/steps/CalculoEnvioSteps.java
@@ -0,0 +1,53 @@
+package com.circulosiete.curso.testing.clase04.steps;
+
+import com.circulosiete.curso.testing.clase04.ShippingCostCalculator;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CalculoEnvioSteps {
+
+  private BigDecimal totalCarrito;
+  private BigDecimal costoMinimo;
+  private ShippingCostCalculator shippingCostCalculator;
+
+  @Given("que el cliente tiene un carrito con valor de {int} pesos y el costo minimo para envio gratis es {int}")
+  public void given(Integer totalCarrito, Integer costoAnonimo) {
+    this.totalCarrito = BigDecimal.valueOf(totalCarrito);
+    this.costoMinimo = BigDecimal.valueOf(costoAnonimo);
+
+    this.shippingCostCalculator = new ShippingCostCalculator(
+      this.costoMinimo,
+      BigDecimal.valueOf(50)
+    );
+
+  }
+
+  @When("el cliente procede al pago")
+  public void when() {
+    // Aquí se podría llamar al método que calcula el costo de envío
+    // ShippingCostCalculator.calculateShippingCost(totalCarrito, costoMinimo);
+
+    var costoEnvio = this.shippingCostCalculator.calculate(
+      this.totalCarrito
+    );
+  }
+
+  @Then("el costo de envío debe ser {int}")
+  public void then(Integer costoEnvio) {
+    // Aquí se podría verificar que el costo de envío es 0
+    // assertEquals(BigDecimal.ZERO, ShippingCostCalculator.calculateShippingCost(totalCarrito, costoMinimo));
+
+    assertThat(
+      costo
+
+      this.costoEnvio).isEqualByComparingTo(BigDecimal.valueOf()
+    )
+  }
+
+}
