e8d0badd8d43492c88b9d2623ea6d08f88a681cc
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-08-14 19:40:48 -0600

    add implementation on method transfer

--- clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
+++ clase03/labs/src/main/java/com/circulosiete/curso/testing/clase03/TransferService.java
@@ -1,14 +1,27 @@
 package com.circulosiete.curso.testing.clase03;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 public class TransferService {
   public List<TransferCreated> getEvents() {
     return List.of();
   }
 
-  public Transfer transfer(Account from, Account to, BigDecimal bigDecimal) {
-    return null;
+  public Transfer transfer(Account from, Account to, BigDecimal amountToTransfer) {
+    var comparison = from.getBalance().compareTo(amountToTransfer);
+    if (comparison < 0) {
+      throw new IllegalStateException("Insufficient funds");
+    }
+
+    var result =  new Transfer(
+      UUID.randomUUID(),
+      from.getUuid(),
+      to.getUuid(),
+      amountToTransfer,
+      LocalDateTime.now());
+    return result;
   }
 }
